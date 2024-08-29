a45b129e67ad065102bc7e3bdace1062f51ef191
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-08-29 13:59:13 +0700

    add new pet section not functionality

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,9 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import Header from "../../components/Home/Header";
 import Slider from "../../components/Home/Slider";
 import PetListByCategory from "../../components/Home/PetListByCategory";
-
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Colors from "../../constants/Colors";
 const Home = () => {
     return (
         <View style={{ padding: 20, marginTop: 20 }}>
@@ -14,8 +15,35 @@ const Home = () => {
             {/*List of PETS*/}
             <PetListByCategory></PetListByCategory>
             {/*Add New Pet options*/}
+            <TouchableOpacity style={styles.addNewPetContainer}>
+                <MaterialIcons name="pets" size={24} color="black" />
+                <Text
+                    style={{
+                        fontFamily: "outfit-medium",
+                        fontSize: 18,
+                    }}
+                >
+                    Add New Pet
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 };
 
 export default Home;
+const styles = StyleSheet.create({
+    addNewPetContainer: {
+        display: "flex",
+        flexDirection: "row",
+        gap: 10,
+        alignItems: "center",
+        marginTop: 20,
+        padding: 20,
+        backgroundColor: Colors.LIGHT_PRIMARY,
+        borderWidth: 1,
+        borderColor: Colors.PRIMARY,
+        borderRadius: 15,
+        borderStyle: "dashed",
+        justifyContent: "center",
+    },
+});
